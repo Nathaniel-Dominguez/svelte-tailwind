ac6d29b809a204ad0d1a028c205b77e616159712
Nate Dominguez <nathaniel.a.dominguez.com>
2023-09-28 15:06:45 -0700

    finished the tutorial base project, svelte and tailwind work with easy data binding and store for a basic todo app.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,6 @@
 <script>
 	import "../app.css";
-	import Navbar from "../components/Navbar.svelte"
 </script>
-<Navbar/>
 <div class="container mx-auto my-6 max-w-lg">
 	<slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,4 +8,7 @@
 <main>
    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">Todo</h1>
    <TodoForm/>
+   {#each $todos as todo}
+        <Todo todo={todo} index={todo.id} />
+   {/each}
 </main>
