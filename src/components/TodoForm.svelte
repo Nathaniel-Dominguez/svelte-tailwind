f2bf93b3b210e1ef666f82828aa780826d4cbce5
Nate Dominguez <nathaniel.a.dominguez.com>
2023-09-28 14:42:19 -0700

    added todostore.js, started connecting it to todoForm. Realized it needed to be moved into src.

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,7 +1,9 @@
 <script>
+    import {addTodo } from "svelte-tailwind/stores/todoStore.js";
     let todo = '';
-    
     const handleSubmit = () => {
+        addTodo(todo);
+        todo = '';
         console.log("submitting")
     }
 
@@ -9,7 +11,7 @@
 
 <form class="my-6" on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-sm mb-2">
-        <label for="todo"></label>
+        <label class="font-bold mb-2" for="todo">Todo</label>
         <input type="text" bind:value={todo} name="todo" placeholder="Watcha gotta do?" class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" />
     </div>
     <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
